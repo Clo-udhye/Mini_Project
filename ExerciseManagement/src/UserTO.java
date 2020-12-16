eaeeb5be5bf11969add1af8fc570cc0d1694c5a8
ParkDahye <clo_udhye@naver.com>
2020-12-16 23:21:15 +0900

    [Add] UserTO, MemberTO, TscheduleTO

---
+++ ExerciseManagement/src/UserTO.java
@@ -0,0 +1,26 @@
+
+public class UserTO {
+	
+	private String id;
+	private String password;
+	private String type;
+	
+	public String getId() {
+		return id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+}
